bb7117b65395a96d205730376d1b767903edab0d
YevheniiDimov <83459259+YevheniiDimov@users.noreply.github.com>
2023-08-13 08:57:23 +0100

    Init react app

---
+++ client/src/components/Grid.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+
+const Button = styled.button``
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
